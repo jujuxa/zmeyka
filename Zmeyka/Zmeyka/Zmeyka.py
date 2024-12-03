d72360b4d998a8025cdd73e933fc55cbf671c120
jujuxa <mr.zombie6547@gmail.com>
2024-12-03 18:29:44 +0700

    При врезании в стену, змейка появляется с другой стороны

--- Zmeyka/Zmeyka/Zmeyka.py
+++ Zmeyka/Zmeyka/Zmeyka.py
@@ -1,18 +1,23 @@
 import pygame
 pygame.init()
-disp=pygame.display.set_mode((1200,900))
- 
+disp_width = 1200
+disp_height = 900
+disp=pygame.display.set_mode((disp_width,disp_height))
+
 pygame.display.set_caption('Zmeyka')
  
 blue=(0,0,255)
 red=(255,0,0)
 green=(0,255,0)
-x_snake = 300
-y_snake = 300
+snake_x = 300
+snake_y = 300
  
-x_snake_change = 0       
-y_snake_change = 0
- 
+snake_x_change = 0       
+snake_y_change = 0
+
+snake_body = 10
+snake_speed = 15
+
 clock = pygame.time.Clock()
 
 game_over=False
@@ -24,23 +29,30 @@ while not game_over:
             game_over=True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x_snake_change = -10
-                y_snake_change = 0
+                snake_x_change = -10
+                snake_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                x_snake_change = 10
-                y_snake_change = 0
+                snake_x_change = 10
+                snake_y_change = 0
             elif event.key == pygame.K_UP:
-                y_snake_change = -10
-                x_snake_change = 0
+                snake_y_change = -10
+                snake_x_change = 0
             elif event.key == pygame.K_DOWN:
-                y_snake_change = 10
-                x_snake_change = 0
- 
-    x_snake += x_snake_change
-    y_snake += y_snake_change
+                snake_y_change = 10
+                snake_x_change = 0
+    if snake_x >= disp_width:
+        snake_x = 1
+    if snake_x < 0:
+        snake_x = disp_width-1
+    if snake_y >= disp_height:
+        snake_y = 0        
+    if snake_y < 0:
+        snake_y = disp_height-1
+
+    snake_x += snake_x_change
+    snake_y += snake_y_change
     disp.blit(pygame.image.load("fon.png"), ( 0,0 ) )
-    pygame.draw.circle(disp,green,[x_snake,y_snake],15)
-    
+    pygame.draw.circle(disp,green,[snake_x,snake_y],15)
     pygame.display.update()
 pygame.quit()
 quit()
