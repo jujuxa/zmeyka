0064c71454b9ae54b2ab865c488d978fce269a2e
jujuxa <mr.zombie6547@gmail.com>
2024-12-20 13:37:11 +0700

    Добавлено несколько видов яблок

--- Zmeyka/Zmeyka/Zmeyka.py
+++ Zmeyka/Zmeyka/Zmeyka.py
@@ -12,7 +12,7 @@ clock = pygame.time.Clock()
 blue=(0,0,255)
 red=(255,0,0)
 green=(0,255,0)
-gold=(255,215,0)
+black=(0,0,0)
 
 snake_speed = 15
 
@@ -20,13 +20,14 @@ snake_speed = 15
 snake_tex = pygame.image.load("snake_head.png")
 body_tex = pygame.image.load("snake_body.png")
 apple_red_tex = pygame.image.load("apple_red.png")
-
-
+apple_gold_tex = pygame.image.load("apple_gold.png")
+apple_black_tex=pygame.image.load("apple_black.png")
+apple_not_tex=pygame.image.load("apple_not.png")
 
 style_font = pygame.font.SysFont("serif", 25)
 
 def print_score(score):
-    disp.blit(pygame.font.SysFont("cmmi10", 35).render('Ваш счёт: '+ str(score),True,gold),[10,10])
+    disp.blit(pygame.font.SysFont("cmmi10", 35).render('Ваш счёт: '+ str(score),True,black),[10,10])
 def text(message, color):
     mesg = style_font.render(message, True, color)
     disp.blit(mesg, [50, 100])
@@ -54,9 +55,10 @@ def gameLoop():
 
     score= 0
 
+    apple_choose=random.randint(1,100)
 
-    apple_x = int(round(random.randrange(0, disp_width -35-35) / 10.0) * 10.0)
-    apple_y = int(round(random.randrange(0, disp_height-35 - 35) / 10.0) * 10.0)
+    apple_x = int(round(random.randrange(0, disp_width -70) / 10.0) * 10.0)
+    apple_y = int(round(random.randrange(0, disp_height-70) / 10.0) * 10.0)
 
 
     game_over=False
@@ -105,6 +107,15 @@ def gameLoop():
             snake_y = disp_height-1
         
         
+        if 85<apple_choose<=100:
+            disp.blit(apple_gold_tex,[apple_x,apple_y])
+        elif 0<apple_choose<=20:
+            disp.blit(apple_not_tex,[apple_x,apple_y])
+        elif 40<apple_choose<=55:
+            disp.blit(apple_black_tex,[apple_x,apple_y])
+        else:
+            disp.blit(apple_red_tex,[apple_x,apple_y])
+        
         snake_x += snake_x_t
         snake_y += snake_y_t
 
@@ -125,10 +136,81 @@ def gameLoop():
         
 
         if ((0<=(snake_x -apple_x)<35) or (0<=(apple_x -snake_x)<35)) and ((0<=(snake_y - apple_y)<35) or (0<=(apple_y -snake_y)<35)):
-            apple_x = int(round(random.randrange(0, disp_width - 35-35) / 10.0) * 10.0)
-            apple_y = int(round(random.randrange(0, disp_height - 35-35) / 10.0) * 10.0)
-            snake_length+= 1
-            score+= 1
+            if 85<apple_choose<=100:
+                apple_x = int(round(random.randrange(0, disp_width - 70) / 10.0) * 10.0)
+                apple_y = int(round(random.randrange(0, disp_height - 70) / 10.0) * 10.0)
+                apple_choose = random.randint(1,100)
+                if snake_length >1:
+                    snake_length -= 1
+                    del snake[0]
+                score+= 5
+            elif 0<apple_choose<=20:
+                if snake_length>1:
+                    if snake_y_t!=0:
+                        if snake_y_t<0:
+                            if apple_y+10*snake_speed>disp_height:
+                                for i in range(10):
+                                    snake.insert(0,[snake[0][0],snake[0][1]-1])
+                            else:
+                                for i in range(10):
+                                    snake.insert(0,[snake[-1][0],snake[-1][1]+1])
+                        if snake_y_t>0:
+                            if apple_y-10*snake_speed<0:
+                                for i in range(10):
+                                    snake.insert(0,[snake[0][0],snake[0][1]+1])
+                            else:
+                                for i in range(10):
+                                    snake.insert(0,[snake[-1][0],snake[-1][1]-1])
+                    else:
+                        if snake_x_t>0:
+                            if apple_x+10*snake_speed>disp_width:
+                                for i in range(10):
+                                    snake.insert(0,[snake[0][0]-1,snake[0][1]])
+                            else:
+                                for i in range(10):
+                                    snake.insert(0,[snake[-1][0]+1,snake[-1][1]])
+                        if snake_x_t<0:
+                            if apple_x-10*snake_speed<0:
+                                for i in range(10):
+                                    snake.insert(0,[snake[0][0]+1,snake[0][1]])
+                            else:
+                                for i in range(10):
+                                    snake.insert(0,[snake[-1][0]-1,snake[-1][1]])
+                else:
+                    if snake_y_t!=0:
+                        if apple_y+10*snake_speed>disp_height:
+                            for i in range(10):
+                               snake.insert(0,[snake[0][0],snake[0][1]-1])
+                        else:
+                            for i in range(10):
+                               snake.insert(0,[snake[0][0],snake[0][1]+1])
+                    else:
+                        if apple_x+10*snake_speed>disp_width:
+                            for i in range(10):
+                                snake.insert(0,[snake[0][0]-1,snake[0][1]])
+                        else:
+                             for i in range(10):
+                                 snake.insert(0,[snake[0][0]+1,snake[0][1]])
+                apple_x = int(round(random.randrange(0, disp_width - 70) / 10.0) * 10.0)
+                apple_y = int(round(random.randrange(0, disp_height - 70) / 10.0) * 10.0)
+                apple_choose = random.randint(1,100)
+                snake_length += 10
+                score +=0    
+            elif 40<apple_choose<=55:
+                apple_x = int(round(random.randrange(0, disp_width - 70) / 10.0) * 10.0)
+                apple_y = int(round(random.randrange(0, disp_height - 70) / 10.0) * 10.0)
+                apple_choose = random.randint(1,100)
+                if snake_length >1:
+                    while(len(snake)>1):
+                        del snake[0]
+                    snake_length = 1
+                score+= 15
+            else:
+                apple_x = int(round(random.randrange(0, disp_width - 70) / 10.0) * 10.0)
+                apple_y = int(round(random.randrange(0, disp_height - 70) / 10.0) * 10.0)
+                apple_choose = random.randint(1,100)
+                snake_length+= 1
+                score+= 1
         
         pygame.display.update()
         clock.tick(15)
